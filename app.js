6b4769962a33e2202525c964b894034041a5394a
NushK650 <Nush8653@gmail.com>
2025-01-31 09:44:58 -0800

    last js update

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-import { saveToLocalStorageByName, getLocalStorage, removeFromLocalStorage } from "./localStorage.js";
+import {
+  saveToLocalStorageByName,
+  getLocalStorage,
+  removeFromLocalStorage,
+} from "./localStorage.js";
 
-
+const shiny = document.getElementById("shiny")
 const search = document.getElementById("search");
 const pokemonName = document.getElementById("pokemonName");
 const pokemonLocation = document.getElementById("pokemonLocation");
@@ -15,76 +19,106 @@ const element = document.getElementById("element");
 const close = document.getElementById("close");
 const info = document.getElementById("info");
 const favoritesSection = document.getElementById("favoritesSection");
+let shinyImg = "";
+let defaultImg = "";
 
 async function getData(pokemon) {
   try {
-    
-    
     const response = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
     );
     const data = await response.json();
-   
-    
+
     const response2 = await fetch(data.location_area_encounters);
     const data2 = await response2.json();
-   
 
-    const speciesResponse = await fetch(data.species.url); // gets the data by drilling into the url 
-const speciesData = await speciesResponse.json(); // just a response like the one above 
-const evolutionUrl = speciesData.evolution_chain.url; //just the url
+    const speciesResponse = await fetch(data.species.url);
+    const speciesData = await speciesResponse.json();
+    const evolutionUrl = speciesData.evolution_chain.url;
 
-const evolutionResponse = await fetch(evolutionUrl); //goes into the url from above 
-const evolutionData = await evolutionResponse.json(); // where the real data is 
+    const evolutionResponse = await fetch(evolutionUrl);
+    const evolutionData = await evolutionResponse.json();
 
-   
-    console.log(evolutionData.chain);
+    let evoStage = evolutionData.chain;
+    let evoChain = [evoStage.species.name];
 
-    let evoStage = evolutionData.chain; // stores the chain into a variable 
-    let evoChain = [evoStage.species.name]; // drills into the chain 
-    while (evoStage.evolves_to.length > 0) { // while loop to find all possible evolutions 
-      evoChain.push(...evoStage.evolves_to.map(evo => evo.species.name)); // spread operater to put the names in its own arguements and then the .map to only get the names  
-      evoStage = evoStage.evolves_to[0]; 
+    for (let i = 0; i < evoStage.evolves_to.length; i++) {
+      evoChain.push(evoStage.evolves_to[i].species.name);
+      for (let j = 0; j < evoStage.evolves_to[i].evolves_to.length; j++) {
+        evoChain.push(evoStage.evolves_to[i].evolves_to[j].species.name);
+      }
     }
 
     let moveNames = "";
-for (let i = 0; i < data.moves.length; i++) {
-  moveNames += data.moves[i].move.name;
-  if (i < data.moves.length - 1) {
-    moveNames += ", "; 
-  }
-}
+    for (let i = 0; i < data.moves.length; i++) {
+      moveNames += data.moves[i].move.name;
+      if (i < data.moves.length - 1) {
+        moveNames += ", ";
+      }
+    }
 
-let abilitiesNames = "";
-for(let i = 0; i < data.abilities.length; i++){
-  abilitiesNames += data.abilities[i].ability.name;
-  if(i < data.abilities.length - 1){
-    abilitiesNames += ", ";
-  }
-}
+    let abilitiesNames = "";
+    for (let i = 0; i < data.abilities.length; i++) {
+      abilitiesNames += data.abilities[i].ability.name;
+      if (i < data.abilities.length - 1) {
+        abilitiesNames += ", ";
+      }
+    }
 
-element.innerText = `Element: ${data.types[0].type.name}`;
-    pokemonName.innerText = data.forms[0].name.toUpperCase();
-    pokemonImg.src = data.sprites.front_default;
-    pokemonLocation.innerText = `Location: ${data2[0].location_area.name.replace(/-/g," ")}`;
-    moves.innerText = `Moves: ${moveNames}`;
-    abilities.innerText = `Abilities: ${abilitiesNames}`; 
-    evo.innerText = `Evolution Chain: ${evoChain.join(", ")}`; 
+    let favorites = getLocalStorage();
 
+    if (!favorites.includes(pokemon)) {
+      fav.src = "./assets/pokeball.png";
+    } else if(favorites.includes(pokemon)){
+      fav.src = "./assets/pokeball (1).png";
+    }
 
+    shinyImg = data.sprites.front_shiny;
+   defaultImg = data.sprites.front_default;
+   
+   if(data.id < 650){
+     element.innerText = `Element: ${data.types[0].type.name}`;
+     pokemonName.innerText = data.forms[0].name.toUpperCase();
+     pokemonImg.src = data.sprites.front_default;
+     pokemonLocation.innerText = `Location: ${data2[0].location_area.name.replace(/-/g," ")}`;
+     moves.innerText = `Moves: ${moveNames}`;
+     abilities.innerText = `Abilities: ${abilitiesNames}`;
+     evo.innerText = `Evolution Chain: ${evoChain.join(", ")}`;
+
+   } else{
+    pokemonName.innerText = "Your Pokemon is not in our Pokedex!"
+   }
+    
   } catch (error) {
-    alert("Error");
+    console.log("Error", error.message);
   }
+  
 }
 
-close.addEventListener("click", ()=>{
+shiny.addEventListener("click",()=>{
+  if(pokemonImg.src == shinyImg)
+    {
+    pokemonImg.src = defaultImg
+    shiny.src = "./assets/star (1).png" 
+  }else{
+    pokemonImg.src = shinyImg;
+    shiny.src = "./assets/star.png";
+  }
+});
+
+getData(); 
+
+
+
+close.addEventListener("click", () => {
   favoritesSection.classList.add("hidden");
-})
+});
 
 search.addEventListener("keydown", async (event) => {
   if (event.key === "Enter") {
     let pokemon = search.value;
     await getData(pokemon);
+    search.value = "";
   }
 });
 
@@ -93,55 +127,47 @@ random.addEventListener("click", () => {
   getData(pokemon);
 });
 
-function getFavorites()
-{
-  
-  let favoritesList= getLocalStorage();
+function getFavorites() {
+  let favoritesList = getLocalStorage();
   info.innerHTML = "";
   console.log(favoritesList);
-  favoritesList.map(fav => {
-    let option = document.createElement("div"); 
-    
-    option.innerHTML = `<p>${fav}</p>`; 
-    
-    let deletebtn = document.createElement('button');
-    deletebtn.type = 'button';
+  favoritesList.map((fav) => {
+    let option = document.createElement("div");
+
+    option.innerHTML = `<p class="flex">${fav}</p>`;
+
+    let deletebtn = document.createElement("button");
+    deletebtn.type = "button";
     deletebtn.className = "w-[10px] h-[10px] flex justify-center";
     let deleteIcon = document.createElement("img");
     deleteIcon.src = "./assets/cancel.png";
     deleteIcon.alt = "Delete";
 
     deletebtn.appendChild(deleteIcon);
-  
-    deletebtn.addEventListener('click', function () {
-        removeFromLocalStorage(fav); 
-        option.remove(); 
+
+    deletebtn.addEventListener("click", function () {
+      removeFromLocalStorage(fav);
+      option.remove();
     });
-  
-    option.addEventListener('click', function(){
+
+    option.addEventListener("click", function () {
       getData(fav);
     });
-    
-    option.appendChild(deletebtn); 
-    info.appendChild(option); 
+
+    option.appendChild(deletebtn);
+    info.appendChild(option);
   });
 }
 
-fav.addEventListener("click", () => {
+fav.addEventListener("click", async () => {
   let pokemon = pokemonName.innerText;
-  if (!pokemon) {
-    alert("Please enter a Pokémon name.");
-    return;
-  }
-
   let favorites = getLocalStorage();
   if (favorites.includes(pokemon)) {
     removeFromLocalStorage(pokemon);
-    fav.src = "./assets/pokeball.png"; 
   } else {
     saveToLocalStorageByName(pokemon);
-    fav.src = "./assets/pokeball (1).png"; 
   }
+  await getData(pokemon);
 });
 
 favList.addEventListener("click", () => {
